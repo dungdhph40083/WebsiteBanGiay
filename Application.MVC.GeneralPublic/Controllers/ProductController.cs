1a8284edf7cdbee3e4ffe6bfd2d62fa39a12a202
Tuanhung01z <phamhungpv1104@gmail.com>
2024-12-06 16:58:04 +0700

    Phân trang Product

--- Application.MVC.GeneralPublic/Controllers/ProductController.cs
+++ Application.MVC.GeneralPublic/Controllers/ProductController.cs
@@ -1,6 +1,11 @@
-﻿using Application.Data.ModelContexts; // Namespace của DbContext
-using Application.Data.Models; // Namespace của Product model
+﻿using Application.Data.ModelContexts;
+using Application.Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Application.MVC.GeneralPublic.Controllers
 {
@@ -8,18 +13,32 @@ namespace Application.MVC.GeneralPublic.Controllers
     {
         private readonly GiayDBContext _context;
         HttpClient client;
+
         public ProductController(GiayDBContext context)
         {
             _context = context;
             client = new HttpClient();
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int page = 1)
         {
-            var productss = await client.GetFromJsonAsync<List<Product>>("https://localhost:7187/api/Product/get-all");
-            ViewBag.Products = productss ?? new List<Product>();
-            var products = _context.Products.ToList();
-            return View(products);
+            int pageSize = 9;
+            var products = await client.GetFromJsonAsync<List<Product>>("https://localhost:7187/api/Product/get-all");
+
+            if (products != null)
+            {
+                int totalProducts = products.Count();
+                int totalPages = (int)Math.Ceiling(totalProducts / (double)pageSize);
+
+                var pagedProducts = products.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+                ViewBag.Products = pagedProducts;
+                ViewBag.CurrentPage = page;
+                ViewBag.TotalPages = totalPages;
+            }
+
+            return View();
         }
+
     }
 }
