18506dcc477bd0f3bfcdf8980ce1951f7f6c8edd
somerandostuff <133385141+somerandostuff@users.noreply.github.com>
2024-12-16 20:49:42 +0700

    HOÀN THÀNH CÁI GIỎ HÀNG (vẫn còn fake data) LET'S GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- Application.MVC.GeneralPublic/Controllers/UserCartController.cs
+++ Application.MVC.GeneralPublic/Controllers/UserCartController.cs
@@ -10,9 +10,13 @@ namespace Application.MVC.GeneralPublic.Controllers
     {
         HttpClient Client = new HttpClient();
 
+        // Fake data; xóa sau khi có Đăng nhập/Đăng ký!!!
+        // USER ID LÀ MỘT USER ĐỊNH SẴN NÊN NÓ CŨNG LÀ FAKE DATA
+        Guid UserID = Guid.Parse("bbd122d1-8961-4363-820e-3ad1a87064e4");
+
         public async Task<ActionResult> Index()
         {
-            string URL = $@"HTTPS://LOCALHOST:7187/API/SHOPPINGCART";
+            string URL = $@"HTTPS://LOCALHOST:7187/API/SHOPPINGCART/USER/{UserID}";
 
             var Response = await Client.GetFromJsonAsync<List<ShoppingCart>>(URL);
             return View(Response);
@@ -21,15 +25,11 @@ namespace Application.MVC.GeneralPublic.Controllers
         public async Task<ActionResult> Add2Cart(Guid? ID, int? Quantity, bool? AdditionMode)
         {
             // TODO:
-            // 1. nút cập nhật giỏ hàng (HARD)
+            // 1. nút cập nhật giỏ hàng (HARD) DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
             // 2. nút xóa (EASY)  DONE
             // 3. căn chỉnh (NORMAL)  DONE
             // 4. thêm cột thông tin (size) (EASY)  DONE
             // 5. làm thông báo thay đổi chưa được lưu (EASY)  DONE
-
-
-            // Fake data; xóa sau khi có Đăng nhập/Đăng ký!!!
-            Guid UserID = Guid.Parse("bbd122d1-8961-4363-820e-3ad1a87064e4");
 
             if (ID != null)
             {
@@ -50,9 +50,16 @@ namespace Application.MVC.GeneralPublic.Controllers
             else return RedirectToAction(nameof(Index));
         }
 
-        public ActionResult UpdateWholeCart(List<ShoppingCart> Response)
+        public async Task<ActionResult> UpdateWholeCart(List<ShoppingCart> BigCart)
         {
-            Console.WriteLine(JsonConvert.SerializeObject(Response, Formatting.Indented));
+            foreach (var Item in BigCart)
+            {
+                string URL =
+        $@"https://localhost:7187/api/ShoppingCart/Add2Cart/{UserID}/{Item.ProductDetailID}?Quantity={Item.QuantityCart ?? 0}&AdditionMode=false";
+
+                var Response = await Client.PutAsync(URL, null);
+            }
+
             return RedirectToAction(nameof(Index));
         }
     }
