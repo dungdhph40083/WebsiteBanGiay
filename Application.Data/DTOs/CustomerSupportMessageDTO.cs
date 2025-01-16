9a854d80ec74e2fc8e4dda369bd08ef18b51e825
Phạm Tuấn Hùng <148750714+TuanHung01z@users.noreply.github.com>
2025-01-17 03:30:09 +0700

    Update CustomerSupportMessageDTO.cs

--- Application.Data/DTOs/CustomerSupportMessageDTO.cs
+++ Application.Data/DTOs/CustomerSupportMessageDTO.cs
@@ -1,13 +1,21 @@
-﻿namespace Application.Data.DTOs
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Data.DTOs
 {
     public class CustomerSupportMessageDTO
     {
         public Guid MessageID { get; set; }
+        [Required(ErrorMessage = "Bắt buộc phải nhập lời nhắn")]
         public string? MessageContent { get; set; }
         public Guid? UserID { get; set; }
         public DateTime? CreatedAt { get; set; }
+        [Required(ErrorMessage = "Bắt buộc phải nhập Tên")]
         public string? FirstName { get; set; }
+        [EmailAddress(ErrorMessage = "Email không hợp lệ!")]
+        [Required(ErrorMessage = "Bắt buộc phải nhập Email")]
         public string? Email { get; set; }
+        [Required(ErrorMessage = "Số điện thoại là bắt buộc.")]
+        [RegularExpression(@"^(03|05|07|08|09)\d{8}$", ErrorMessage = "Số điện thoại không hợp lệ.")]
         public string? PhoneNumber { get; set; }
         public byte? Status { get; set; }
 
